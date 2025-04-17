de3d1dfb432f97447f09618b51a0dcd4750d974a
n1klas4008 <45050975+hawolt@users.noreply.github.com>
2025-04-17 20:36:01 +0200

    track more variables to ensure a safe render process

--- src/main/java/com/hawolt/gotr/events/RenderSafetyEvent.java
+++ src/main/java/com/hawolt/gotr/events/RenderSafetyEvent.java
@@ -5,11 +5,27 @@ import lombok.Getter;
 
 public class RenderSafetyEvent {
     @Getter(AccessLevel.PUBLIC)
-    private final boolean isVolatileState, isInGame, isMinigameWidgetVisible;
+    private final boolean isVolatileState, isInGame, isWidgetAvailable, isWidgetVisible;
 
-    public RenderSafetyEvent(boolean isVolatileState, boolean isInGame, boolean isMinigameWidgetVisible) {
-        this.isMinigameWidgetVisible = isMinigameWidgetVisible;
+    public RenderSafetyEvent(
+            boolean isVolatileState,
+            boolean isInGame,
+            boolean isWidgetAvailable,
+            boolean isWidgetVisible
+    ) {
+        this.isWidgetAvailable = isWidgetAvailable;
         this.isVolatileState = isVolatileState;
+        this.isWidgetVisible = isWidgetVisible;
         this.isInGame = isInGame;
+    }
+
+    @Override
+    public String toString() {
+        return "RenderSafetyEvent{" +
+                "isVolatileState=" + isVolatileState +
+                ", isInGame=" + isInGame +
+                ", isWidgetAvailable=" + isWidgetAvailable +
+                ", isWidgetVisible=" + isWidgetVisible +
+                '}';
     }
 }
